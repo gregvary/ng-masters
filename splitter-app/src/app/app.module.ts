dae37d101d125e145b75dc3b5824ac55a551db9d
gregvary <gregvary@gmail.com>
2017-12-06 18:30:55 +0100

    Erstellen des Material Module und Hinzufügen des BrowserAnimationModule

--- splitter-app/src/app/app.module.ts
+++ splitter-app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
