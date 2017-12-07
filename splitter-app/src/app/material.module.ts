9841a9c58e1e89c817b31b508b9e3c7787e283dc
gregvary <gregvary@gmail.com>
2017-12-07 14:44:59 +0100

    Added basic toolbar

--- splitter-app/src/app/material.module.ts
+++ splitter-app/src/app/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
-    CommonModule
+    MatToolbarModule
   ],
+  exports: [
+  	MatToolbarModule
+  	],
   declarations: []
 })
 export class MaterialModule { }
