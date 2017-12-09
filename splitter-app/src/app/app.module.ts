889e6b4b1bd4aa817dfd2aa65f624cffc9c019ed
Fehler40 <skaya.moskow@gmail.com>
2017-12-09 15:15:00 +0100

    minor changes

--- splitter-app/src/app/app.module.ts
+++ splitter-app/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< Updated upstream
 
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule } from '@angular/forms';
->>>>>>> Stashed changes
+
 
 import { AppComponent } from './app.component';
 
@@ -17,15 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-<<<<<<< Updated upstream
     BrowserModule
-=======
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
     FormsModule
->>>>>>> Stashed changes
+
   ],
   providers: [],
   bootstrap: [AppComponent]
